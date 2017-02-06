9089e4035ca7c0263578e6dbb6b63352c01b29b1
Randall Pruim <rpruim@gmail.com>
2017-02-05 23:18:58 -0500

    comment out roxygen

--- R/densify.R
+++ R/densify.R
@@ -1,4 +1,17 @@
-
+#' Turn "rasterized" kernel into "rasterized" density function
+#'
+#' Turn "rasterized" kernel into "rasterized" density function
+#'
+#' @param x,y numerical vectors of equal length giving the (x, y) coordinates
+#'        of a kernel function \code{k} with \code{k(x) = y}.
+#' @return a rescaled version of \code{y} that turns the kernel into a pdf.
+#' @examples
+#' x <- seq(0, 1, by = 0.05)
+#' y1 <- (x - 0.5)^2
+#' y2 <- densify(x, y1)
+#' if (require(lattice)) {
+#'   xyplot(y1 + y2 ~ x, type = "l")
+#' }
 
 densify <- function(x, y) {
   if ( diff(range(diff(x))) > 0.1 * min(abs(diff(x))) )
