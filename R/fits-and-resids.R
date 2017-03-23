ace0ab7f3066eb4eb79a18a2b3af6506c1fcbee9
Randall Pruim <rpruim@gmail.com>
2017-03-23 09:54:50 -0400

    compute fitted() and resid() using coef() when n=1

--- R/fits-and-resids.R
+++ R/fits-and-resids.R
@@ -1,10 +1,24 @@
-fitted.map <- function(object, n = 1e3, data = object@data, ..., na.rm = TRUE) {
-  apply(link(object, n = n, data = data, ...), 2, mean, na.rm = na.rm)
+fitted.map <- function(object, n = 1, data = NULL, post = NULL, ...,
+                       na.rm = TRUE) {
+  if (is.null(data)) data <- object@data
+
+  if (is.null(post)) {
+    if (n == 1) {
+      apply(link(object, post = coef(object), data = data, ...),
+            2, mean, na.rm = na.rm)
+    } else {
+      apply(link(object, n = n, data = data, ...), 2, mean, na.rm = na.rm)
+    }
+  } else {
+    apply(link(object, post = post, data = data, ...), 2, mean, na.rm = na.rm)
+  }
 }
 
-residuals.map <- function(object, n = 1e3, data = object@data, fits = NULL, ..., na.rm = TRUE){
+residuals.map <-
+  function(object, n = 1e3, data = NULL, fits = NULL, post = NULL, ..., na.rm = TRUE){
+
   if (is.null(fits)) {
-    fits <- fitted(object, n=n, data = data, ..., na.rm = na.rm)
+    fits <- fitted(object, n=n, data = data, post = post, ..., na.rm = na.rm)
   }
   outcome_name <- object@formula[[1]][[2]]
   message("Residuals computed assuming outcome = ", as.character(outcome_name))
