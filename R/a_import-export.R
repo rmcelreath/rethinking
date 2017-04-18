9c002f7264d4f6e5d762be3f0cb9962a4382d96a
Randall Pruim <rpruim@gmail.com>
2017-04-18 09:33:45 -0400

    put all import/export into a file

---
+++ R/a_import-export.R
@@ -0,0 +1,15 @@
+#' @importFrom grDevices col2rgb densCols devAskNewPage hsv pdf quartz rgb rgb2hsv
+#' @importFrom graphics abline arrows axis contour curve dotchart image lines mtext points polygon text xspline
+#' @importFrom stats BIC as.formula cor cov cov2cor dbeta dbinom density dgamma
+#' @importFrom stats dnbinom dnorm dpois fitted gaussian model.frame model.matrix
+#' @importFrom stats optim predict qnorm quantile rbeta rbinom resid rexp rgamma rnbinom
+#' @importFrom stats rnorm rpois sd terms var
+#' @importFrom utils read.csv
+#' @importFrom MASS mvrnorm
+#' @importClassesFrom rstan stanfit
+#' @importFrom rstan sflist2stanfit stan
+#' @importFrom rstan extract traceplot
+#' @importFrom mvtnorm dmvnorm rmvnorm
+#' @importFrom grDevices dev.new
+
+NULL
