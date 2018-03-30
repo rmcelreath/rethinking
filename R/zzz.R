6de4fcd23fe56388e27ebe72fd0bed374d6fb505
Robert Link <robert.link@pnnl.gov>
2018-03-30 08:29:55 -0400

    Fix errors thrown in package check.

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 
 .onAttach <- function(...) {
   theLib <- dirname(system.file(package = "rethinking"))
-  pkgdesc <- packageDescription("rethinking", lib.loc = theLib)
+  pkgdesc <- utils::packageDescription("rethinking", lib.loc = theLib)
   builddate <- gsub(';.*$', '', pkgdesc$Packaged)
   msg <- paste("rethinking (Version ", pkgdesc$Version, ")", sep = "")
   packageStartupMessage(msg)
