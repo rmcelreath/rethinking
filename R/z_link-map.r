1bea9e578851415b5e93de370bcb7ef9aec9c199
Robert Link <robert.link@pnnl.gov>
2018-03-30 08:18:43 -0400

    Avoid converting guessed formula to string in link-map.
    In cases where the map object `links` slot is empty, converting the
    expression guessed from the formula into a string corrupted the
    expression.  The conversion turns out to be necessary, so we skip it.
    
    Fixes #120

--- R/z_link-map.r
+++ R/z_link-map.r
@@ -8,7 +8,7 @@ function( fit , data , n=1000 , ... ) {
 
 setMethod("link", "map",
 function( fit , data , n=1000 , post , refresh=0.1 , replace=list() , flatten=TRUE , ... ) {
-    
+
     if ( class(fit)!="map" ) stop("Requires map fit")
     if ( missing(data) ) {
         data <- fit@data
@@ -17,21 +17,21 @@ function( fit , data , n=1000 , post , refresh=0.1 , replace=list() , flatten=TR
         # weird vectorization errors otherwise
         #data <- as.data.frame(data)
     }
-    
-    if ( missing(post) ) 
+
+    if ( missing(post) )
         post <- extract.samples(fit,n=n)
     else {
         n <- dim(post[[1]])[1]
         if ( is.null(n) ) n <- length(post[[1]])
     }
-    
+
     # replace with any elements of replace list
     if ( length( replace ) > 0 ) {
         for ( i in 1:length(replace) ) {
             post[[ names(replace)[i] ]] <- replace[[i]]
         }
     }
-    
+
     nlm <- length(fit@links)
     f_do_lm <- TRUE
     if ( nlm==0 ) {
@@ -40,28 +40,28 @@ function( fit , data , n=1000 , post , refresh=0.1 , replace=list() , flatten=TR
         nlm <- 1
         f_do_lm <- FALSE
     }
-    
+
     link_out <- vector(mode="list",length=nlm)
-    
+
     # for each linear model, compute value for each sample
     for ( i in 1:nlm ) {
         ref_inc <- floor(n*refresh)
         ref_next <- ref_inc
-        
+
         if ( f_do_lm==TRUE ) {
             parout <- fit@links[[i]][[1]]
-            lm <- fit@links[[i]][[2]]
+            lm <- parse( text=fit@links[[i]][[2]] )
         } else {
             parout <- "ll"
             lm <- "0"
             # hacky solution -- find density function and insert whatever expression in typical link spot
-            flik <- as.character(fit@formula[[1]][[3]][[1]])
+            flik <- fit@formula[[1]][[3]][[1]]
             # mu for Gaussian
-            if ( flik=="dnorm" ) lm <- as.character( fit@formula[[1]][[3]][[2]] )
+            if ( flik=="dnorm" ) lm <- fit@formula[[1]][[3]][[2]]
             # p for binomial -- assume in third spot, after size
-            if ( flik=="dbinom" ) lm <- as.character( fit@formula[[1]][[3]][[3]] )
+            if ( flik=="dbinom" ) lm <- fit@formula[[1]][[3]][[3]]
             # lambda for poisson
-            if ( flik=="dpois" ) lm <- as.character( fit@formula[[1]][[3]][[2]] )
+            if ( flik=="dpois" ) lm <- fit@formula[[1]][[3]][[2]]
         }
         # empty matrix to hold samples-by-cases values of linear model
         value <- matrix(NA,nrow=n,ncol=length(data[[1]]))
@@ -76,7 +76,7 @@ function( fit , data , n=1000 , post , refresh=0.1 , replace=list() , flatten=TR
                     if ( ref_next > n ) ref_next <- n
                 }
             }
-        
+
             # make environment
             init <- list() # holds one row of samples across all params
             for ( j in 1:length(post) ) {
@@ -91,17 +91,17 @@ function( fit , data , n=1000 , post , refresh=0.1 , replace=list() , flatten=TR
             }#j
             e <- list( as.list(data) , as.list(init) )
             e <- unlist( e , recursive=FALSE )
-            value[s,] <- eval(parse(text=lm),envir=e)
+            value[s,] <- eval(lm,envir=e)
         }#s
         link_out[[i]] <- value
         names(link_out)[i] <- parout
     }
-    
+
     if ( refresh>0 ) cat("\n")
-    
+
     if ( flatten==TRUE )
         if ( length(link_out)==1 ) link_out <- link_out[[1]]
-    
+
     return(link_out)
 }
 )
