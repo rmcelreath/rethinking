89cf75eed610d9177ad9e1512b249434dcb6f026
Richard McElreath <rmcelreath@gmail.com>
2020-07-26 11:28:23 +0200

    Update cmdstan_support.r
    a little more cmdstanr support. still needs tests!

--- R/cmdstan_support.r
+++ R/cmdstan_support.r
@@ -14,7 +14,7 @@ cmdstanr_model_write <- function( the_model ) {
     }
 
 # wrapper to fit stan model using cmdstanr and return rstan stanfit object
-cstan <- function( file , model_code , data=list() , chains=1 , cores=1 , iter=1000 , threads=1 , control=list(adapt_delta=0.95) , cpp_options=list() , ... ) {
+cstan <- function( file , model_code , data=list() , chains=1 , cores=1 , iter=1000 , warmup , threads=1 , control=list(adapt_delta=0.95) , cpp_options=list() , save_warmup=TRUE , ... ) {
 
     if ( threads>1 ) cpp_options[['stan_threads']] <- TRUE
 
@@ -25,11 +25,42 @@ cstan <- function( file , model_code , data=list() , chains=1 , cores=1 , iter=1
     require(cmdstanr,quietly=TRUE)
     mod <- cmdstan_model( file , compile=TRUE , cpp_options=cpp_options )
 
-    if ( threads > 1 )
-        cmdstanfit <- mod$sample( data=data , chains=chains , parallel_chains=cores , iter_sampling=floor(iter/2) , adapt_delta=as.numeric(control[['adapt_delta']]) , threads_per_chain=threads , ... )
-    else
-        cmdstanfit <- mod$sample( data=data , chains=chains , parallel_chains=cores , iter_sampling=floor(iter/2) , adapt_delta=as.numeric(control[['adapt_delta']]) , ... )
+    if ( missing(warmup) ) {
+        samp <- floor(iter/2)
+        warm <- floor(iter/2)
+    } else {
+        samp <- iter - warmup
+        warm <- warmup
+    } 
 
+    # pull out any control arguments
+    carg_adapt_delta <- 0.95
+    if ( !is.null( control[['adapt_delta']] ) )
+        carg_adapt_delta <- as.numeric(control[['adapt_delta']])
+    carg_max_treedepth <- 11
+    if ( !is.null( control[['max_treedepth']] ) )
+        carg_max_treedepth <- as.numeric(control[['max_treedepth']])
+
+    # sample
+    if ( threads > 1 )
+        cmdstanfit <- mod$sample( data=data , 
+            chains=chains , 
+            parallel_chains=cores , 
+            iter_sampling=samp , iter_warmup=warm , 
+            adapt_delta=carg_adapt_delta , 
+            max_treedepth=carg_max_treedepth , 
+            threads_per_chain=threads ,
+            save_warmup=save_warmup , ... )
+    else
+        cmdstanfit <- mod$sample( data=data , 
+            chains=chains , 
+            parallel_chains=cores , 
+            iter_sampling=samp , iter_warmup=warm , 
+            adapt_delta=carg_adapt_delta , 
+            max_treedepth=carg_max_treedepth ,
+            save_warmup=save_warmup , ... )
+
+    # coerce to stanfit object
     stanfit <- rstan::read_stan_csv(cmdstanfit$output_files())
 
     return(stanfit)
